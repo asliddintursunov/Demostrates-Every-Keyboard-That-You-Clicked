6789f3a38ebc250c80e54d69ebc9000679e775ee
asliddintursunov <asliddintursunovfarkhodovich@gmail.com>
2023-01-29 01:10:46 +0500

    Added some new code

--- keyEvent.js
+++ keyEvent.js
@@ -2,18 +2,6 @@ let container = document.getElementById('container');
 
 
 window.addEventListener('keyup', (e) => {
-    // key.style = 'display: none';
-    // keyValue.textContent = e.key;
-    // keyCode.textContent = e.keyCode;
-    // code.textContent = e.code;
-
-    // container.appendChild(keyValue)
-    // container.appendChild(keyCode)
-    // container.appendChild(code)
-
-    // keyValue.style = 'display: block;';
-    // keyCode.style = 'display: block;';
-    // code.style = 'display: block;';
     container.innerHTML = `
     <div class="key">
         ${e.key == " " ? "Space" : e.key}
